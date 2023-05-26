0c204d0143befc54b06caa27a9bc814b74a1f4c6
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-05-26 16:35:51 +0700

    menambahkan variable

--- DoubleLinkedList_184/DoubleLinkedList_184.cpp
+++ DoubleLinkedList_184/DoubleLinkedList_184.cpp
@@ -30,3 +30,9 @@ DoubleLinkedList::DoubleLinkedList() {
 }
 
 void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student : ";
+	cin >> nim;
+	cout << "\nEnter the name of the student : ";
+	cin >> nm;
