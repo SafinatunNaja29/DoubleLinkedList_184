c685a52a28662ebb24558d2da980cfcdfbab43b4
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-05-26 16:44:19 +0700

    menyisipkan sebuah node di antara dua node dalam linked list.

--- DoubleLinkedList_184/DoubleLinkedList_184.cpp
+++ DoubleLinkedList_184/DoubleLinkedList_184.cpp
@@ -53,3 +53,8 @@ void DoubleLinkedList::addNode() {
 		START = newNode;			//step 6
 		return;
 	}
+
+	/*Inserting a Node Between Two Nodes in the List*/
+	Node* current = START;			//step 1.a
+	Node* previous = NULL;			//step 1.b
+	while (current->next != NULL && current->next->NoMhs < nim)
